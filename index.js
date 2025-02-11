9ffa097417f3dd5ee0eb4a71b994e6fea6996f2b
b3liott <eliott.georges8@gmail.com>
2025-02-11 12:04:37 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const fs = require('fs');
+
+
+
+app.commandLine.appendSwitch('ignore-certificate-errors');
+app.disableHardwareAcceleration();
+
+
+let config = { thehive: "http://192.168.5.249:9000", wazuh: "https://192.168.5.252", proxmox: "https://192.168.5.253:8006", n8n: "http://192.168.5.251:5678" };
+
+
+app.whenReady().then(() => {
+
+    const templatePath = path.join(__dirname, 'index_template.html');
+    let templateHtml = fs.readFileSync(templatePath, 'utf-8');
+
+    templateHtml = templateHtml
+        .replace("{{thehive}}", config.thehive)
+        .replace("{{wazuh}}", config.wazuh)
+        .replace("{{proxmox}}", config.proxmox)
+        .replace("{{n8n}}", config.n8n);;
+
+    fs.writeFileSync(path.join(__dirname, 'index.html'), templateHtml);
+
+    const win = new BrowserWindow({
+        width: 1920,
+        height: 1080,
+        webPreferences: {
+            nodeIntegration: true,
+            webviewTag: true,
+            contextIsolation: true
+        }
+    });     
+
+    win.loadFile('index.html');
+});
